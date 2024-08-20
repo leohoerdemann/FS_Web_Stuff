fdc796cffe476252fc18f1b3c16c176a7ce7357d
Leo Hoerdemann <leo.h.trip@gmail.com>
2024-08-20 17:55:55 -0500

    added a whole bunch of stuff like websocket, redid main page, db stuff, etc

--- FS_Web_Stuff.Server/DBInteract.cs
+++ FS_Web_Stuff.Server/DBInteract.cs
@@ -1,6 +1,20 @@
 ﻿namespace FS_Web_Stuff.Server
 {
+    using Google.Cloud.Firestore;
+
     public static class DBInteract
     {
+
+        public static FirestoreDb db = FirestoreDb.Create("fs-web-stuff");
+
+
+
+        public static async Task AddDocument(string collection, object data)
+        {
+            DocumentReference docRef = db.Collection(collection).Document();
+            await docRef.SetAsync(data);
+        }
+
+
     }
 }
